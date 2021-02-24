ff0eaeb1c88874a2becd77c130611ca01660d7b0
Dustin Specker <dustin.specker@att.com>
2021-02-24 17:25:46 -0600

    feat: enable development pipeline to pass behind proxy with TLS
    This is a squashed commit, keeping messages intact for history.
    
    - feat(charts/development-pipeline): work behind corporate proxy
    
    dockerd sidecar works behind proxy with cert. Proxy is only needed on
    the sidecar to pull public images. The cert is mounted via a host path
    so that the proxy may be trusted.
    
    - fix(standard-container/roles): remove installing Helm push plugin
    
    The standard-container Dockerfile already installs the Helm push plugin,
    so no reason to try to install it in multiple ansible roles.
    
    I suspect this was originally done because someone tried to use `helm
    push` in the ansible role, but Helm couldn't find even though it was
    installed in the image. But tekton defines a the HOME env var if you
    describe the pod in a cluster. So if we just define HELM_DATA_HOME to
    the location where the push plugin is installed we can remove having to
    install it in the ansible roles.
    
    - feat(standard-container/roles): use image.image_from for docker build
    
    Change-Id: Ibc3c5f400978cb98d2d2a37b737b56125f4c2aa7

--- tools/gate/jarvis/700-deploy-jarvis-system.sh
+++ tools/gate/jarvis/700-deploy-jarvis-system.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -ex
 
+# escape commas in no_proxy because Helm tries to split the value on commas
 # shellcheck disable=SC2046
 helm upgrade \
     --create-namespace \
     --install \
     --namespace=jarvis-system \
+    --set proxy.http_proxy="$http_proxy" \
+    --set proxy.https_proxy="$https_proxy" \
+    --set proxy.no_proxy="$(echo $no_proxy | sed "s/,/\\\,/g")" \
+    --set proxy.internal_certs_dir="$PWD/tools/gate/jarvis/ubuntu-base/internal-certs/" \
     jarvis-system \
     "./charts/jarvis-system" \
     $(./tools/deployment/common/get-values-overrides.sh jarvis-system)
