8377c131d9df6c2b10a656fdbbe7be6db595c4d9
Tin Lam <tin@irrational.io>
2021-03-15 13:24:27 -0500

    fix(overrides): place back missing overrides
    This patch places back in functionality introduced in [0] that were lost
    from refactoring.
    
    [0] https://review.opendev.org/c/airship/charts/+/775065
    
    Signed-off-by: Tin Lam <tin@irrational.io>
    Change-Id: I6671fcf36c2fe0867c7bb7886cf24d50c09cfad0

--- tools/gate/jarvis/standard-container/assets/jarvis/functional_deploy.sh
+++ tools/gate/jarvis/standard-container/assets/jarvis/functional_deploy.sh
@@ -4,7 +4,8 @@ set -ex
 
 update-ca-certificates
 ansible-playbook -vvv /playbooks/functional-microflow.yaml -i hosts \
- -e '{"stage":"deploy"}' \
- -e @"/workspace/development_pipeline_data/default.json" \
- -e @"/workspace/development_pipeline_data/cluster.json" \
- -e 'loop_source="/workspace/development_pipeline_data/chart.json"'
+  -e '{"stage":"deploy"}' \
+  -e @"/workspace/development_pipeline_data/default.json" \
+  -e @"/workspace/development_pipeline_data/cluster.json" \
+  -e 'loop_source="/workspace/development_pipeline_data/chart.json"' \
+  -e '{"datapath":"/workspace/development_pipeline_data"}'
